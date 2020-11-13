524a83f0b64d881bf56d2adf736648bffde56baa
LaNice Powell <lanicemp@aol.com>
2020-11-13 01:02:36 -0500

    upated the import and fixed mapStateToProps

--- rare_charcuterie-app/src/App.js
+++ rare_charcuterie-app/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from 'react-redux';
 import "./App.css";
 import { fetchItems } from './actions/itemActions';
-import ItemList from './components/ItemList';
+import ItemsContainer from './containers/ItemsContainer'
 
 class App extends Component {
   componentDidMount() {
@@ -14,7 +14,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1> Rare Chacuterie </h1>
-        <ItemList itemPics={this.props.itemPics} />
+        <ItemsContainer itemPics={this.props.itemPics} />
 
       </div>
     );
@@ -22,8 +22,8 @@ class App extends Component {
 }
 const mapStateToProps = state => {
   return {
-    itemPics: state.items,
-    loading: state.loading
+    items: state.items
+    
   }
 }
 const mapDispatchToProps = dispatch => {
@@ -33,57 +33,3 @@ const mapDispatchToProps = dispatch => {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
-//   state = {
-//     products: [
-//       {name: 'Max', price: 50}, 
-//       {name: ' Date', price: 30},
-//       {name: 'Single', price: 25}
-//     ],
-//     otherState: ' some other value'
-//   }
-
-// switchNameHandler = (newName) => {
-//   this.setState({
-//     persons: [
-//       {name: newName, price:50},
-//       {name: ' Date', price: 30},
-//       {name: 'Single', price: 25}
-//     ]
-//   }) 
-   
-//   }
-//   nameChangedHandler = (event) => {
-//     this.setState( {
-//       persons: [
-//         { name: 'Max', age: 28 },
-//         { name: event.target.value,  price: 30 },
-//         { name: 'Single', price: 25}
-//       ]
-//     } )
-//   }
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1> Rare Chacuterie </h1>
-//         <p>This is working!</p>
-//         <button onClick={()=>this.switchNameHandler('Cheese Board')}>Switch Name</button>
-//         <Product
-//          name={this.state.products[0].name} 
-//          price={this.state.products[0].price}/>
-//         <Product
-//          name={this.state.products[1].name} 
-//          price={this.state.products[1].price}
-//          click={this.switchNameHandler.bind(this, 'Max!')} 
-//          changed={this.nameChangedHandler} >My Hobbies: Racing</Product>
-//         <Product
-//          name={this.state.products[2].name} 
-//          price={this.state.products[2].price}/>
-        
-        
-
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
