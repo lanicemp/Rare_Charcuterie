37dac3ee600546d4d64b15936975357b3a8e3e0d
LaNice Powell <lanicemp@aol.com>
2020-11-14 07:16:11 -0500

    created home component

---
+++ rare_charcuterie-app/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+class Home extends React.Component {
+
+    render() {
+      return (
+        <div className="Home">
+            <br></br><br></br>
+            Rare Charcuterie
+            <br></br><br></br>
+        </div>
+      );
+    }
+  }
+
+  export default Home
