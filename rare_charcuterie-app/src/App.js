f6534164cb448de3eeaa78b8301529db7a2a7d4f
LaNice Powell <lanicemp@aol.com>
2020-11-14 07:17:24 -0500

    added imports and moved header to components

--- rare_charcuterie-app/src/App.js
+++ rare_charcuterie-app/src/App.js
@@ -3,20 +3,26 @@ import { connect } from 'react-redux';
 import "./App.css";
 import { fetchItems } from './actions/itemActions';
 import ItemsContainer from './containers/ItemsContainer'
+import Home from './components/Home'
+import Header from './components/Header'
+import { Switch, Route, withRouter } from 'react-router-dom'
 
 class App extends Component {
   componentDidMount() {
     this.props.fetchItems()
   }
-  
+      
   render() {
    
     return (
+      
       <div className="App">
-        <h1> Rare Chacuterie </h1>
+       <Header/>
         <ItemsContainer itemPics={this.props.itemPics} />
+        <Route exact path='/' component={Home}></Route>
 
       </div>
+      
     );
   }
 }
@@ -32,4 +38,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default  withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
