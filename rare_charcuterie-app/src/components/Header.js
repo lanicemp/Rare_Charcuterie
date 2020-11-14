7cde2ff6d2251c2173020e65bc64644813bd8600
LaNice Powell <lanicemp@aol.com>
2020-11-14 07:15:52 -0500

    created header component

---
+++ rare_charcuterie-app/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { NavLink } from 'react-router-dom'
+
+class Header extends React.Component {
+
+  render() {
+      return (
+        <div >
+           <header>
+               <h1> Rare Chacuterie </h1>
+        </header>   
+              
+             
+      {/* <button id="sort-dresses">Sort Dresses </button> */}
+            
+        </div>
+      );
+    }
+  }
+
+  export default Header
