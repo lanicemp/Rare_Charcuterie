7dcd3ac0d95f9885edc1c8b348c2c926ecd1f650
LaNice Powell <lanicemp@aol.com>
2020-11-14 19:22:56 -0500

    added navigation

---
+++ rare_charcuterie-app/src/components/Navigation.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class Navigation extends React.Component {
+  render() {
+    return (
+      <div className="Navigaiton">
+        <ul>
+          <li>
+            <a class="active" href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#news">News</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
