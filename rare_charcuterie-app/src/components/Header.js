ebd9a0af282c53b81392b25948454319682c3362
LaNice Powell <lanicemp@aol.com>
2020-11-15 14:49:28 -0500

    moved button

--- rare_charcuterie-app/src/components/Header.js
+++ rare_charcuterie-app/src/components/Header.js
@@ -1,27 +1,24 @@
 import React from "react";
 import "../styles/Header.css";
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 class Header extends React.Component {
-
-
-
   render() {
-      return (
-        <div className="Header">
-           <header>
-               <h1> Rare Charcuterie </h1>
-               
-               <button>Cart</button>     
-        </header>   
-        
-              
-             
-      {/* <button id="sort-dresses">Sort Dresses </button> */}
-            
-        </div>
-      );
-    }
-  }
+    return (
+      <div className="Header">
+        <header>
+          <h1> Rare Charcuterie </h1>
+          {/* <button>Cart</button> */}
+        </header>
+        <section className="Header-section">
+        <button>Cart</button>
+        </section>
+      
 
-  export default Header
+        {/* <button id="sort-dresses">Sort Dresses </button> */}
+      </div>
+    );
+  }
+}
+
+export default Header;
