42fcd59e4ff0f642f076180e5ac4a80bfed4e6f6
LaNice Powell <lanicemp@aol.com>
2020-11-14 19:20:33 -0500

    Added rating action

---
+++ rare_charcuterie-app/src/actions/ratingActions.js
@@ -0,0 +1,12 @@
+export const fetchRatingss = () => {
+    return (dispatch) => {
+     
+        dispatch({type:"FETCH_RATINGS"})
+        fetch("http://localhost:3000/api/v1/ratings")
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          return dispatch({ type: "ADD_RATINGS", ratings: data });
+        });
+    };
+  };
