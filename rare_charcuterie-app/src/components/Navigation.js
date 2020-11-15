7206a44df77010842c33b8e698997cc44a7eba04
LaNice Powell <lanicemp@aol.com>
2020-11-15 14:37:09 -0500

    added and updated navbar

--- rare_charcuterie-app/src/components/Navigation.js
+++ rare_charcuterie-app/src/components/Navigation.js
@@ -8,18 +8,21 @@ class Navigation extends React.Component {
       <div className="Navigaiton">
         <ul>
           <li>
-            <a class="active" href="#home">
+            <a className="active" href="#home">
               Home
             </a>
           </li>
           <li>
-            <a href="#news">News</a>
+            <a href="#reviews">Reviews</a>
           </li>
           <li>
             <a href="#contact">Contact</a>
           </li>
           <li>
             <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#faqs">FAQs</a>
           </li>
         </ul>
       </div>
