cf44e550f27315bde491574644594c5fc3c60c48
LaNice Powell <lanicemp@aol.com>
2020-11-15 01:54:13 -0500

    working on the search bar funtionality

--- rare_charcuterie-app/src/components/Home.js
+++ rare_charcuterie-app/src/components/Home.js
@@ -1,17 +1,44 @@
 import React from "react";
-
+import SearchBar from "./SearchBar";
+import ItemList from "./ItemList";
 
 class Home extends React.Component {
+  // SearchPage = () => {
+  //   const [input, setInput] = useState("");
+  //   const [itemListDefault, setItemListDefault] = useState();
+  //   const [itemList, setItemList] = useState();
+  // };
+  // fetchData = async () => {
+  //   return await fetch("https://restcountries.eu/rest/v2/all")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setItemList(data);
+  //       setItemListDefault(data);
+  //     });
+  // };
 
-    render() {
-      return (
-        <div className="Home">
-            <br></br><br></br>
-            Rare Charcuterie
-            <br></br><br></br>
-        </div>
-      );
-    }
+  // updateInput = async (input) => {
+  //   const filtered = countryListDefault.filter((country) => {
+  //     return country.name.toLowerCase().includes(input.toLowerCase());
+  //   });
+  //   setInput(input);
+  //   setCountryList(filtered);
+  // };
+
+  render() {
+    return (
+      <div className="Home">
+        <br></br>
+        <br></br>
+        <SearchBar  />
+        <button >Search</button>
+        {/* <SearchBar input={input} onChange={updateInput} /> */}
+        {/* <button >Search</button> */}
+        <br></br>
+        <br></br>
+      </div>
+    );
   }
+}
 
-  export default Home
+export default Home;
