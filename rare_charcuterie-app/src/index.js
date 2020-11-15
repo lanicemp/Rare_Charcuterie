cf810791d34ddc3e714de9c988e579d19f7eacb2
LaNice Powell <lanicemp@aol.com>
2020-11-15 01:50:30 -0500

    added app.scss an bootstrap import

--- rare_charcuterie-app/src/index.js
+++ rare_charcuterie-app/src/index.js
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './App.scss'
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
 import reportWebVitals from './reportWebVitals';
 
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+
+
 import { BrowserRouter } from "react-router-dom";
 import itemsReducer from "./reducers/itemsReducer.js";
 
-const store = createStore(itemsReducer, applyMiddleware(thunk));
+
+
+ const store = createStore(itemsReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
