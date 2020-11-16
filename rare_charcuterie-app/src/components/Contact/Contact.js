84f6c95505e175545302bf321219a77fe87e6a2b
LaNice Powell <lanicemp@aol.com>
2020-11-16 13:36:20 -0500

    created component

---
+++ rare_charcuterie-app/src/components/Contact/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <div className="contact">
+        <h2> Contact </h2>
+        
+
+        {/* <button id="sort-dresses">Sort Dresses </button> */}
+      </div>
+    );
+  }
+}
+
+export default Contact;
