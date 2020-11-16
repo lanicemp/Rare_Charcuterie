f7328165fa21215e985803471b98c649e97220c5
LaNice Powell <lanicemp@aol.com>
2020-11-16 13:35:19 -0500

    moved into folder

---
+++ rare_charcuterie-app/src/components/Footer/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { NavLink } from 'react-router-dom'
+
+class Footer extends React.Component {
+
+  render() {
+      return (
+        <div className ='footer' >
+           <footer>
+           <ul>
+          <li>
+            <a className="active" href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#news">News</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+        </ul>
+        </footer>   
+              
+             
+      {/* <button id="sort-dresses">Sort Dresses </button> */}
+            
+        </div>
+      );
+    }
+  }
+
+  export default Footer
