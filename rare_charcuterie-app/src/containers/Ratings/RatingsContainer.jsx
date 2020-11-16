0ad422424fe492a5428f6162939300a15ae7972c
LaNice Powell <lanicemp@aol.com>
2020-11-16 13:30:52 -0500

    moved into folder

---
+++ rare_charcuterie-app/src/containers/Ratings/RatingsContainer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { connect } from "react-redux";
+import {Route, Switch} from 'react-router-dom'
+import RatingList from "../../components/Rating/RatingList";
+// import { Card, Button } from "react-bootstrap";
+import { fetchRatings } from "../../actions/ratingActions";
+
+class RatingsContainer extends React.Component {
+  componentDidMount() {
+    this.props.fetchRatings();
+  }
+
+  render() {
+    return (
+      <div className='rating_card'>
+      <div className='ratings-container'>
+        <h2>Reviews</h2>
+        <RatingList ratings={this.props.ratings}/>
+       
+      </div>
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state) => {
+   console.log("this is just state for ratings", state);
+
+  return { ratings: state.ratings };
+};
+
+export default connect(mapStateToProps, { fetchRatings })(RatingsContainer);
