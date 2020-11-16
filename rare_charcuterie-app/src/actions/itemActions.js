0e7d668404e170d9447a6bcce552454b4d75bd27
LaNice Powell <lanicemp@aol.com>
2020-11-16 11:31:36 -0500

    console log

--- rare_charcuterie-app/src/actions/itemActions.js
+++ rare_charcuterie-app/src/actions/itemActions.js
@@ -5,6 +5,7 @@ export const fetchItems = () => {
         fetch("http://localhost:3000/api/v1/items")
         .then(response => response.json())
         .then(data => {
+          console.log("in fetch items")
           console.log(data)
           return dispatch({ type: "ADD_ITEMS", items: data });
         });
