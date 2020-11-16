2c8240792bc1aadeb65c04569d125d5ccc92bcfc
LaNice Powell <lanicemp@aol.com>
2020-11-16 16:43:47 -0500

    created about component

---
+++ rare_charcuterie-app/src/components/About/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+class About extends React.Component {
+  render() {
+    return (
+      <div className="about">
+        <h2> About Us </h2>
+        
+
+        {/* <button id="sort-dresses">Sort Dresses </button> */}
+      </div>
+    );
+  }
+}
+
+export default About;
