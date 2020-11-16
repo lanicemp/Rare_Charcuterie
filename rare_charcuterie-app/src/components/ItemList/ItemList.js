d2300365852b223f124bf5fd0622c28e11c02c50
LaNice Powell <lanicemp@aol.com>
2020-11-16 16:38:51 -0500

    updated html

--- rare_charcuterie-app/src/components/ItemList/ItemList.js
+++ rare_charcuterie-app/src/components/ItemList/ItemList.js
@@ -4,16 +4,20 @@ import "./ItemList.css";
 class ItemList extends React.Component {
   renderItems = () => {
     return this.props.items.map((item) => {
+      console.log("in item list");
       console.log(item);
       return (
-        <div key={item.id} className="card">
-          <img src={item.img_url} />
-          <div className="card-body">
-            <h2>{item.name}</h2>
+        <div className="itemList">
+          <h2> Rare Products</h2>
+          <div className="card"key={item.id} >
+            <img src={item.img_url} />
+            <div className="card-body">
+              <h2>{item.name}</h2>
 
-            <p>
-              {item.size} <br></br> {item.price}
-            </p>
+              <p>
+                {item.size} <br></br> {item.price}
+              </p>
+            </div>
           </div>
         </div>
       );
