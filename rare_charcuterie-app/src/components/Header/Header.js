c181968a041df01e6c941a7f171f6f08dcd47be7
LaNice Powell <lanicemp@aol.com>
2020-11-16 11:07:11 -0500

    moved to subfolder updated css import

---
+++ rare_charcuterie-app/src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.css";
+import { NavLink } from "react-router-dom";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="Header">
+        <header>
+          <h1> Rare Charcuterie </h1>
+          {/* <button>Cart</button> */}
+        </header>
+        <section className="Header-section">
+        <button>Cart</button>
+        </section>
+      
+
+        {/* <button id="sort-dresses">Sort Dresses </button> */}
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ rare_charcuterie-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./SearchBar.css";
+
+const SearchBar = ({keyword,setKeyword}) => {
+  
+  return (
+    <input className='BarStyling'
+     key="random1"
+     value={keyword}
+     placeholder={"search boards"}
+     onChange={(e) => setKeyword(e.target.value)}
+    />
+  );
+}
+
+export default SearchBar
