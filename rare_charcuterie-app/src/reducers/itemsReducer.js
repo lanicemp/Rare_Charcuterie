c96f7d9a4e413cd9e097e2f21eef318301146cef
LaNice Powell <lanicemp@aol.com>
2020-11-18 21:13:02 -0500

    updated the items reducer

--- rare_charcuterie-app/src/reducers/itemsReducer.js
+++ rare_charcuterie-app/src/reducers/itemsReducer.js
@@ -1,25 +1,29 @@
-// state = { items: [], loading: false 
-const itemsReducer = (state = [], action) => {
+// state = { items: [], loading: false} 
+const itemsReducer = (state = { items: [], loading: false} , action) => {
   switch (action.type) {
     case "FETCH_ITEMS":
+      debugger 
       console.log("in item reducer");
-      state.push({...action.payload})
-      // return [...state, [...state.items]];
-      return state
-      // {
-      //   ...state,
-      //   items: [...state.items],
-      //   loading: true,
-      // };
+       return {...state, items:action.payload };
+     // action.payload is  the return jsonResopnse from rails. 
+  
     // case "ADD_ITEMS":
       // return {
       //   ...state,
       //   items: action.items,
       //   loading: false,
       // };
-      return [...state, action.items];
+      // case "ADD_ITEM":
+      //   return [...state, action.item];
+   
+      // case "REMOVE_ITEM":
+      //   idx = state.findIndex(item => item.id  === action.id)
+      //   return [...state.slice(0, idx), ...state.slice(idx + 1)];
+   
+      // return [...state, action.items];
     default:
       return state;
   }
 };
+
 export default itemsReducer;
