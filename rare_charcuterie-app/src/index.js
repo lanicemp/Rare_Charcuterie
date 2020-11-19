9eb0a218a2ab23c2d770b0c17cb8f753c31c950f
LaNice Powell <lanicemp@aol.com>
2020-11-18 21:08:50 -0500

    added rootReducer

--- rare_charcuterie-app/src/index.js
+++ rare_charcuterie-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './App.scss'
+// import './App.scss'
 import "./index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
@@ -11,11 +11,12 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { BrowserRouter as Router} from "react-router-dom";
 import reducer from './reducers/index'
+import rootReducer from "./reducers/rootReducer"
 
 
 
 // const reducer = combineReducers(reducers)
- const store = createStore(reducer, applyMiddleware(thunk));
+ const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
