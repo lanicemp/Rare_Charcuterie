5ad548e3edc42dfdb042cc8681dc4bb5d792b80b
LaNice Powell <lanicemp@aol.com>
2020-11-18 21:25:56 -0500

    removed h2 tag

--- rare_charcuterie-app/src/components/ItemList/ItemList.js
+++ rare_charcuterie-app/src/components/ItemList/ItemList.js
@@ -1,30 +1,36 @@
 import React, { Component } from "react";
+// import { connect } from 'react-redux';
+// import { fetchItems } from "../../actions/itemActions";
+//removing the imports above because Im passing it down to the props in the container 
 import "./ItemList.css";
 
-class ItemList extends React.Component {
+class ItemList extends Component {
+ 
   renderItems = () => {
     return this.props.items.map((item) => {
-      console.log("in item list");
-      console.log(item);
+      console.log("line 9")
       return (
-        <div className="itemList">
-          <h2> Rare Products</h2>
-          <div className="card"key={item.id} >
-            <img src={item.img_url} />
-            <div className="card-body">
-              <h2>{item.name}</h2>
-
-              <p>
-                {item.size} <br></br> {item.price}
-              </p>
+        
+         
+          <div className="itemList">
+            <div className="card" key={item.id}>
+              <img src={item.img_url} />
+              <div className="card-body">
+                <h2>{item.name}</h2>
+                <p>
+                  {item.size} <br></br> {item.price}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
+        
       );
     });
   };
   render() {
+  // debugger
     return <div>{this.renderItems()}</div>;
   }
 }
+
 export default ItemList;
