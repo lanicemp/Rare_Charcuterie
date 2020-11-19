422a63b4655b250b6263224e3da2ed1592fabb7f
LaNice Powell <lanicemp@aol.com>
2020-11-18 21:27:14 -0500

    updated to fix functionality

--- rare_charcuterie-app/src/actions/itemActions.js
+++ rare_charcuterie-app/src/actions/itemActions.js
@@ -1,13 +1,21 @@
 export const fetchItems = () => {
     return (dispatch) => {
-     
-        // dispatch({type:"FETCH_ITEMS"})
+        // dispatch({type: "FETCH_ITEMS" })
         fetch("http://localhost:3000/api/v1/items")
-        .then(response => response.json())
-        .then(payload => {
-          console.log("in fetch items")
-          console.log(payload)
-          return dispatch({ type: "FETCH_ITEMS", payload });
+        .then(response => {
+          return response.json()
+        })
+        .then(items  => {
+          //passing the items to the payload on line 10
+        dispatch({ type: "FETCH_ITEMS", payload:items });
+       //dispatch sends information to the reducer
+       // the inforation is how to manipulate the state. 
         });
+        console.log("in fetch items")
+  
     };
-  };
+    
+  };//Actions are the instructions you pass to the reducer
+
+
+  
