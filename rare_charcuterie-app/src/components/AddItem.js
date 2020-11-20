38538c45aa56c858ac259fcc89721c1f283243b6
LaNice Powell <lanicemp@aol.com>
2020-11-20 05:10:14 -0500

    inital commit

---
+++ rare_charcuterie-app/src/components/AddItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+class AddItem extends React.Component {
+    render() {
+      return (
+        <div className="addItem">
+            <h2>In AddItem</h2>
+          
+  
+          {/* <button id="sort-dresses">Sort Dresses </button> */}
+        </div>
+      );
+    }
+  }
+  
+  export default AddItem ;
