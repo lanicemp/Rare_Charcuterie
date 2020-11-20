13e24e91ea89567b7f540a14285dc36fa49ee444
LaNice Powell <lanicemp@aol.com>
2020-11-20 05:09:49 -0500

    imported bootsrap and created columns and rows

--- rare_charcuterie-app/src/containers/items/ItemsContainer.jsx
+++ rare_charcuterie-app/src/containers/items/ItemsContainer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import {Route, Switch} from 'react-router-dom'
+import { Route, Switch } from "react-router-dom";
 // import { fetchItems } from "../../actions/itemActions";
 import { connect } from "react-redux";
 //it invoke an action I have to connect to redux
 import ItemList from "../../components/ItemList/ItemList";
 // import { Card, Button } from "react-bootstrap";
-
+import { Card, Col, Row, CardGroup, CardColumns } from "react-bootstrap";
 
 class ItemsContainer extends React.Component {
   // componentDidMount() {
@@ -16,29 +16,30 @@ class ItemsContainer extends React.Component {
   render() {
     //  debugger
     return (
-      <div className='item_card'>
-      <div className='items-container'>
-     
-      <h2> Rare Products </h2>
-      
-        <ItemList items={this.props.items}/>
-       
-      </div>
+      <div className="item_card">
+        <div className="items-container">
+          <h2> Rare Products </h2>
+          <CardColumns>
+            <Row>
+              <ItemList items={this.props.items} />
+            </Row>
+          </CardColumns>
+        </div>
       </div>
     );
   }
 }
 const mapStateToProps = (state) => {
-   console.log("this is just state for items", state);
-//Access to state 
+  console.log("this is just state for items", state);
+  //Access to state
   return { items: state.items.items };
-  //Global state is the storage container that holds information to be used by many components. 
+  //Global state is the storage container that holds information to be used by many components.
   //props are the same as attibutes of an object
   //map = access to anything within the global state
 };
 
 export default connect(mapStateToProps)(ItemsContainer);
-//connect is connecting react to redux functionality 
+//connect is connecting react to redux functionality
 // The functionality is access to the global state come from mapStatetoProps aka the first argument(always!!!!)
-//and the dispatch function 
-//Dispatch funtionality coms from mapdispatchToProps aka the second argument to connect (always!!!!)
+//and the dispatch function
+//Dispatch funtionality comes from mapdispatchToProps aka the second argument to connect (always!!!!)
