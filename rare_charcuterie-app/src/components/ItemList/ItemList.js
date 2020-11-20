1ca2572744c8cf19572c4603acaed9d9ceb3c744
LaNice Powell <lanicemp@aol.com>
2020-11-20 05:16:48 -0500

    Added imports updated styling

--- rare_charcuterie-app/src/components/ItemList/ItemList.js
+++ rare_charcuterie-app/src/components/ItemList/ItemList.js
@@ -1,34 +1,54 @@
 import React, { Component } from "react";
+import AddItem from "../AddItem";
+import Navigation from "../Navigation/Navigation";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Card, Col, Row, CardGroup, CardColumns,  } from "react-bootstrap";
+
 // import { connect } from 'react-redux';
 // import { fetchItems } from "../../actions/itemActions";
-//removing the imports above because Im passing it down to the props in the container 
+//removing the imports above because Im passing it down to the props in the container
 import "./ItemList.css";
 
 class ItemList extends Component {
- 
   renderItems = () => {
     return this.props.items.map((item) => {
-      console.log("line 9")
+      console.log("line 9");
       return (
-        
-         
-          <div className="itemList">
-            <div className="card" key={item.id}>
-              <img src={item.img_url} />
-              <div className="card-body">
-                <h2>{item.name}</h2>
-                <p>
-                  {item.size} <br></br> {item.price}
-                </p>
-              </div>
-            </div>
-          </div>
-        
+        <div className="itemList">
+          {/* <CardColumns> */}
+            
+            {/* <Row> */}
+              <Card style={{ width: "25rem" }}>
+                {/* <div className="card" key={item.id}> */}
+                <Card.Img variant= "top"
+                  src={item.img_url}
+                  // onClick={
+                  //   <Router>
+                  //     <Link to={AddItem} />
+                  //     <Switch>
+                  //       <Route exact path="../AddItem" component={AddItem} />
+                  //     </Switch>
+                  //   </Router>
+                  // }
+                />
+                <Card.Body>
+                {/* <div className="card-body"> */}
+                  <h2>{item.name}</h2>
+                  <p>
+                    {item.size} <br></br> {item.price}
+                  </p>
+                {/* </div> */}
+                </Card.Body>
+                {/* </div> */}
+              </Card>
+            {/* </Row> */}
+          {/* </CardColumns> */}
+        </div>
       );
     });
   };
   render() {
-  // debugger
+    // debugger
     return <div>{this.renderItems()}</div>;
   }
 }
