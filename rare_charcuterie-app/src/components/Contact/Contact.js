899b605a850399d5155b12e543ad9d81672440b6
LaNice Powell <lanicemp@aol.com>
2020-11-20 05:17:47 -0500

    updated html

--- rare_charcuterie-app/src/components/Contact/Contact.js
+++ rare_charcuterie-app/src/components/Contact/Contact.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-
-
 class Contact extends React.Component {
   render() {
     return (
       <div className="contact">
-        <h2> Contact </h2>
+        <h2> Contact Us </h2>
+        <div class="container">
         
-
-        {/* <button id="sort-dresses">Sort Dresses </button> */}
-      </div>
+            <p>Swing by for a cup of coffee, or leave us a message:</p>
+          </div>
+        </div>
+      
     );
   }
 }
