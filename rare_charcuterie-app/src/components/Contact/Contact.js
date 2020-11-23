ba02c8bd99e3ac7a191643702b2e49717e90b2cd
LaNice Powell <lanicemp@aol.com>
2020-11-23 10:48:54 -0500

    fixed class

--- rare_charcuterie-app/src/components/Contact/Contact.js
+++ rare_charcuterie-app/src/components/Contact/Contact.js
@@ -5,7 +5,7 @@ class Contact extends React.Component {
     return (
       <div className="contact">
         <h2> Contact Us </h2>
-        <div class="container">
+        <div className="container">
         
             <p>Swing by for a cup of coffee, or leave us a message:</p>
           </div>
