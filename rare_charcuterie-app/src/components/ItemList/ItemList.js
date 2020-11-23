5134898f7572627096676341056082a8657b658d
LaNice Powell <lanicemp@aol.com>
2020-11-23 10:47:39 -0500

    added function for indivual view

--- rare_charcuterie-app/src/components/ItemList/ItemList.js
+++ rare_charcuterie-app/src/components/ItemList/ItemList.js
@@ -1,8 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+
 import AddItem from "../AddItem";
+
 import Navigation from "../Navigation/Navigation";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Card, Col, Row, CardGroup, CardColumns,  } from "react-bootstrap";
+import { Card, Button, Modal } from "react-bootstrap";
 
 // import { connect } from 'react-redux';
 // import { fetchItems } from "../../actions/itemActions";
@@ -10,39 +12,43 @@ import { Card, Col, Row, CardGroup, CardColumns,  } from "react-bootstrap";
 import "./ItemList.css";
 
 class ItemList extends Component {
+  
+  returnViewItem = () => {
+    
+
+    return (
+      //  The Modal -->
+      <div id="myModal" class="modal">
+        {/* <!-- Modal content --> */}
+        <div class="modal-content">
+          <span class="close">&times;</span>
+          <p>Some text in the Modal..</p>
+        </div>
+      </div>
+    );
+  };
+ 
+
   renderItems = () => {
     return this.props.items.map((item) => {
-      console.log("line 9");
+      console.log(document.getElementById);
+
+      // debugger
       return (
-        <div className="itemList">
-          {/* <CardColumns> */}
-            
-            {/* <Row> */}
-              <Card style={{ width: "25rem" }}>
-                {/* <div className="card" key={item.id}> */}
-                <Card.Img variant= "top"
-                  src={item.img_url}
-                  // onClick={
-                  //   <Router>
-                  //     <Link to={AddItem} />
-                  //     <Switch>
-                  //       <Route exact path="../AddItem" component={AddItem} />
-                  //     </Switch>
-                  //   </Router>
-                  // }
-                />
-                <Card.Body>
-                {/* <div className="card-body"> */}
-                  <h2>{item.name}</h2>
-                  <p>
-                    {item.size} <br></br> {item.price}
-                  </p>
-                {/* </div> */}
-                </Card.Body>
-                {/* </div> */}
-              </Card>
-            {/* </Row> */}
-          {/* </CardColumns> */}
+        <div className="itemList" id={item.id} data-id={item.id}>
+          <Card key={item.id} style={{ width: "25rem" }}>
+            <Card.Img key="img" variant="top" src={item.img_url} />
+            <Card.Body key="body">
+              <h2>{item.name}</h2>
+              <h3>$ {item.price}</h3>
+              <Button
+                data-id={item.id}
+                id={item.id}
+                value={item.id}
+                onClick={this.returnViewItem()}
+              > View Item </Button>
+            </Card.Body>
+          </Card>
         </div>
       );
     });
