e6d514260f76e271d8ed0741dca7709821e81fb4
LaNice Powell <lanicemp@aol.com>
2020-11-23 10:39:55 -0500

    added bootstrap card

--- rare_charcuterie-app/src/components/AddItem.js
+++ rare_charcuterie-app/src/components/AddItem.js
@@ -1,17 +1,18 @@
 import React from "react";
-
+import { Card } from "react-bootstrap";
 
 class AddItem extends React.Component {
-    render() {
-      return (
-        <div className="addItem">
-            <h2>In AddItem</h2>
-          
-  
-          {/* <button id="sort-dresses">Sort Dresses </button> */}
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="addItem">
+        <Card>
+            
+          <h2>In AddItem</h2>
+        </Card>
+        {/* <button id="sort-dresses">Sort Dresses </button> */}
+      </div>
+    );
   }
-  
-  export default AddItem ;
+}
+
+export default AddItem;
