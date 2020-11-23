e998d7751d8df3066d24127d3a4a0c3e46985fd6
LaNice Powell <lanicemp@aol.com>
2020-11-23 10:37:12 -0500

    added import

--- rare_charcuterie-app/src/App.js
+++ rare_charcuterie-app/src/App.js
@@ -15,25 +15,31 @@ import Contact from './components/Contact/Contact'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Switch, Route, withRouter } from "react-router-dom";
+import AddItem from "./components/AddItem";
 
 class App extends Component {
   componentDidMount() {
     this.props.fetchItems();
+   
   }
 // /* <RatingsContainer ratingComments={this.props.ratingComments}/> */
   render() {
+    
     return (
       <div className="App">
         <Header />
         <Navigation/>
         <br></br>
         <Home />
+        
         <ItemsContainer ItemList items={this.props.items} />
         <br></br><br></br>
+        
         {/* <About/> */}
         <br></br>
         {/* <Faqs/> */}
       <br></br><br></br>
+     
       <Contact/>
         <Footer/>
         
@@ -42,10 +48,13 @@ class App extends Component {
   }
 }
 const mapStateToProps = state => {
+ 
   return {
     items: state.items,
     ratings:state.ratings
+    
   };
+  
 };
 const mapDispatchToProps = dispatch => {
   return {
