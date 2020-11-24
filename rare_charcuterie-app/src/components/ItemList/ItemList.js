171a0854c101ffa59a6219e2cc7068ede84b9043
LaNice Powell <lanicemp@aol.com>
2020-11-24 18:40:02 -0500

    reorganized and created showModal function

--- rare_charcuterie-app/src/components/ItemList/ItemList.js
+++ rare_charcuterie-app/src/components/ItemList/ItemList.js
@@ -1,10 +1,10 @@
 import React, { Component, useState } from "react";
-
+import ReactDOM from "react-dom";
 import AddItem from "../AddItem";
 
 import Navigation from "../Navigation/Navigation";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Card, Button, Modal } from "react-bootstrap";
+import { Card, Button, Modal, Container } from "react-bootstrap";
 
 // import { connect } from 'react-redux';
 // import { fetchItems } from "../../actions/itemActions";
@@ -14,11 +14,15 @@ import "./ItemList.css";
 class ItemList extends Component {
   constructor(props) {
     super(props);
+    console.log(props);
     this.returnViewItem = this.returnViewItem.bind(this);
     this.handleClose = this.handleClose.bind(this);
+
     this.state = {
       showModal: false,
       item_id: null,
+      selectedItem: null,
+
       //this.state is false so that the modal will not be shown initially.
       // We had to set state becuse the only way to rerender is when state is changed.
       // the state lets react know when to re-render
@@ -26,7 +30,7 @@ class ItemList extends Component {
   }
   renderItems = () => {
     return this.props.items.map((item) => {
-      console.log(document.getElementById);
+      // console.log(document.getElementById);
 
       return (
         <div className="itemList" id={item.id} data-id={item.id}>
@@ -51,35 +55,42 @@ class ItemList extends Component {
     });
   };
 
-  renderItem = (item_id) => {
-    return (
-      <div className="itemList" id={item_id} data-id={item_id}>
-        <Card key={item_id} style={{ width: "25rem" }}>
-          {/* <Card.Img key="img" variant="top" src={item.img_url} /> */}
-          <Card.Body key="body">
-            {/* <h2>{item.name}</h2>
-            <h3>$ {item.price}</h3> */}
-            <Button
-              data-id={item_id}
-              id={item_id}
-              value={item_id}
-              // onClick={this.returnViewItem}
-            >
-              {" "}
-              View Item{" "}
-            </Button>
-          </Card.Body>
-        </Card>
-      </div>
-    );
-  };
-
   handleClose() {
     this.setState({ showModal: false });
   }
+
+  showModal = () => {
+    return (
+      <Modal
+        //  style={{width: "50rem"} }
+        show={this.state.showModal}
+        onHide={() => this.handleClose()}
+        //the other syntax of onHide ={this.handleClose()} created an error of cannot exceed mximum calls to state
+        // changing the call to an arrow function allowed me to get pass this because the functions is
+        //being called in the rended instead of being apart of the render.
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          {this.state.selectedItem.name} In View Item
+          {console.log(this.state)}
+        </Modal.Header>
+        <Modal.Body>{this.renderItem(this.state.item_id)} </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => this.handleClose()}>
+            Close
+          </Button>
+        </Modal.Footer>{" "}
+      </Modal>
+    );
+  };
   returnViewItem(e) {
-    const id = e.target.id;
-    this.setState({ showModal: true, item_id: id });
+    const id = parseInt(e.target.id);
+    const foundItem = this.props.items.find((item) => item.id === id);
+    debugger;
+    this.myRef = React.createRef.bind(id);
+    console.log(this.props.items);
+    this.setState({ showModal: true, item_id: id, selectedItem: foundItem });
     //create const for all of the items that I want passed in this function.
     // const [show, setShow] = useState(false);
 
@@ -92,33 +103,33 @@ class ItemList extends Component {
     //  console.log(e.currentTarget.id)
   }
   render() {
-    // debugger
-
     return (
       <div>
         <div>{this.renderItems()}</div>
-        <Modal
-          show={this.state.showModal}
-          onHide={() => this.handleClose()}
-          //the other syntax of onHide ={this.handleClose()} created an error of cannot exceed mximum calls to state
-          // changing the call to an arrow function allowed me to get pass this because the functions is
-          //being called in the rended instead of being apart of the render.
-          backdrop="static"
-          keyboard={false}
-        >
-          <Modal.Header closeButton>
-            {this.state.item_id} In View Item
-          </Modal.Header>
-          <Modal.Body> {this.renderItem(this.state.item_id)} </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={() => this.handleClose()}>
-              Close
-            </Button>
-          </Modal.Footer>{" "}
-        </Modal>
+
+        {this.state.showModal == true ? this.showModal() : null}
+        {/* {//if this.showModal is true redirect to showModal function or do nothing with null} */}
       </div>
     );
   }
+
+  renderItem = (props, item_id) => {
+    console.log(item_id);
+    console.log(props);
+    return (
+      <div className="viewItem" id={item_id} data-id={item_id}>
+          <Card key={item_id} style={{ width: "25rem" }}>
+          <Card.Body key="body">
+          <Card.Img key="img" variant="top" src={this.state.selectedItem.img_url} />
+          
+            {/* <h2>{item.name}</h2>
+            <h3>$ {item.price}</h3> */}
+            
+            </Card.Body>
+           </Card>
+      </div>
+    );
+  };
 }
 
 export default ItemList;
