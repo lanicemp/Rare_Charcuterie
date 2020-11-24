4fcf7c579b73e0a706d72df62fe993844e773bef
LaNice Powell <lanicemp@aol.com>
2020-11-24 00:12:14 -0500

    added variable and case

--- rare_charcuterie-app/src/reducers/itemsReducer.js
+++ rare_charcuterie-app/src/reducers/itemsReducer.js
@@ -1,29 +1,33 @@
-// state = { items: [], loading: false} 
-const itemsReducer = (state = { items: [], loading: false} , action) => {
+// state = { items: [], loading: false}
+const initialState = {
+  items: [],
+  searchItems: [],
+  loading:false
+};
+const itemsReducer = (state = initialState, action) => {
   switch (action.type) {
+    
     case "FETCH_ITEMS":
-   
       console.log("in item reducer");
-       return {...state, items:action.payload };
-     // action.payload is  the return jsonResopnse from rails. 
-  
-    // case "ADD_ITEMS":
-      // return {
-      //   ...state,
-      //   items: action.items,
-      //   loading: false,
-      // };
-      // case "ADD_ITEM":
-      //   return [...state, action.item];
-   
-      // case "REMOVE_ITEM":
-      //   idx = state.findIndex(item => item.id  === action.id)
-      //   return [...state.slice(0, idx), ...state.slice(idx + 1)];
-   
-      // return [...state, action.items];
+      return { ...state, items: action.payload };
+    // action.payload is  the return jsonResopnse from rails.
+
+    case "ADD_ITEMS":
+      return {
+        ...state,
+        items: [...state.items, action.payload]
+      };
+    
+
+    // case "REMOVE_ITEM":
+    //   idx = state.findIndex(item => item.id  === action.id)
+    //   return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    // return [...state, action.items];
     default:
       return state;
   }
+  
 };
 
 export default itemsReducer;
