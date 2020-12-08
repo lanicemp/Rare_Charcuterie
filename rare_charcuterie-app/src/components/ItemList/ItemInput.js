c3b663c81981c112bb3916e8bf852fef45e90385
LaNice Powell <lanicemp@aol.com>
2020-12-08 07:43:05 -0500

    created form and created compnent did mount

--- rare_charcuterie-app/src/components/ItemList/ItemInput.js
+++ rare_charcuterie-app/src/components/ItemList/ItemInput.js
@@ -1,44 +1,102 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import {addItem} from "../../actions/itemActions"
 
 class ItemInput extends Component {
-state = {
-  name: ""
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      ingredient: "",
+      price: 0,
+      size: "",
+      img_url: ""
+    };
+    // this.handleOnChange = this.handleOnChange.bind(this);
+    // this.handleOnSubmit = this.handleOnSubmit.bind(this);
+  }
 
-handleOnChange = e => {
-  this.setState({
-    name: e.target.value
-  })
-}
+  handleOnChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
 
-handleOnSubmit = e => {
-  e.preventDefault();
-  this.props.addItem(this.state.name)
+    //   let target = e.target;
+    //   let value =  target.value;
+    //   let name = target.name;
+    //   // const ingredient = target.ingredient
+    //   this.setState(
 
-this.setState({
-  name:""
-})
-}
+    //     {[name]: value
+    //       // [ingredient]:value
+    //  }
+    //   );
+  };
 
-
-
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    this.props.addItem(this.state);
+    this.setState({ name: "" }, { ingredient: "" },{price: 0},{size: ""},
+      {img_url: ""} );
+  };
+  componentDidMount() {
+    if (this.props.item) {
+      const { attributes } = this.props.item;
+      this.setState({
+        name: attributes.name,
+        ingredient: attributes.ingredient,
+        price: attributes.price,
+        size: attributes.size
+      });
+    }
+  }
 
   render() {
-    return(
+    let { name, ingredient } = this.state;
+    return (
       <div>
-        Item Input
+        Add New Item
         <form onSubmit={this.handleOnSubmit}>
           <label>Item Name </label>
-          <input type= 'text'
-          name = "name"
-          placeholder= "enter a new name"
-          value = {this.state.name}
-          onChange= { this.handleOnChange}/>
-          <input type = "submit" value = "Add Item"/>
+          <input
+            type="text"
+            name="name"
+            placeholder="enter a new name"
+            value={name}
+            onChange={this.handleOnChange}
+          />
+          <br></br>
+          <label>Ingredients </label>
+          <input
+            type="text"
+            name="ingredient"
+            placeholder="enter a ingredients"
+            value={ingredient}
+            onChange={this.handleOnChange}
+          />
+          <br></br>
+          <label>Price </label>
+          <input
+            type="number"
+            name="price"
+            placeholder="enter a price"
+            value={this.state.price}
+            onChange={this.handleOnChange}
+          />
+          <br></br>
+          <label>Size </label>
+          <input
+            type="text"
+            name="size"
+            size="size"
+            placeholder="enter the size"
+            value={this.state.size}
+            onChange={this.handleOnChange}
+          />
+          <br></br>
+          <input type="submit" value="Add Item" />
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default ItemInput
+export default ItemInput;
