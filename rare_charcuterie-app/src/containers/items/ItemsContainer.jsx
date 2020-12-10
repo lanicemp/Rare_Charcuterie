89bdf7501b039258f67f9ad1297f102d4e014927
LaNice Powell <lanicemp@aol.com>
2020-12-09 23:36:06 -0500

    crated a route and link for Item Input

--- rare_charcuterie-app/src/containers/items/ItemsContainer.jsx
+++ rare_charcuterie-app/src/containers/items/ItemsContainer.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
+import { BrowserRouter as Router, Link, Route, Switch, withRouter } from "react-router-dom";
+import { addItem } from "../../actions/itemActions";
 //it invoke an action I have to connect to redux
-import ItemInput from '../../components/ItemList/ItemInput'
+import ItemInput  from '../../components/ItemList/ItemInput'
 import ItemList from "../../components/ItemList/ItemList";
 import {  Row,  CardColumns, Button } from "react-bootstrap";
 
@@ -10,6 +11,10 @@ class ItemsContainer extends React.Component {
   // componentDidMount() {
   //   this.props.fetchItems();
   // }
+
+  nextPath(path) {
+    this.props.history.push(path);
+  }
   addItem = item => {
     this.props.dispatch({type:'ADD_ITEM', item})
   }
@@ -22,8 +27,13 @@ class ItemsContainer extends React.Component {
       <div className="item_card">
         <div className="items-container">
           <h2> Rare Products </h2>
-          <ItemInput addItem={this.addItem}/>
-          {/* <Button onClick={AddItem}> Add Item </Button > */}
+          {/* <ItemInput addItem={this.addItem} items={this.props.items}/> */}
+          <Router>
+           <Link to = '/ItemInput'> Add Item </Link > 
+             <Switch>
+            <Route exact path="/ItemInput" component={ItemInput} />
+          </Switch>
+           </Router> 
           <CardColumns>
             <Row>
               <ItemList items={this.props.items} />
@@ -37,16 +47,19 @@ class ItemsContainer extends React.Component {
 const mapStateToProps = (state) => {
   console.log("state for items", state);
   //Access to state
-
-
   return { items: state.items.items };
-
   //Global state is the storage container that holds information to be used by many components.
   //props are the same as attibutes of an object
   //map = access to anything within the global state
 };
+const mapdispatchToProps = dispatch => {
+  return {
+    addItem: () => dispatch(addItem())
+  }
 
-export default connect(mapStateToProps)(ItemsContainer);
+}
+
+export default withRouter(connect(mapStateToProps, mapdispatchToProps)(ItemsContainer));
 //connect is connecting react to redux functionality
 // The functionality is access to the global state come from mapStatetoProps aka the first argument(always!!!!)
 //and the dispatch function
