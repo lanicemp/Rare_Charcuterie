0890ab6c9b9b9880a0454c077b241dae86b25fa2
LaNice Powell <lanicemp@aol.com>
2020-12-09 23:31:47 -0500

    updated the resouces

--- rare_charcuterie_api/config/routes.rb
+++ rare_charcuterie_api/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api do 
     namespace:v1 do 
-      resources :items
-      resources :ratings
+      resources :items, only: [:index, :create, :destroy, :update]
+      resources :ratings, only: [:index, :create, :destroy, :update]
     end 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
