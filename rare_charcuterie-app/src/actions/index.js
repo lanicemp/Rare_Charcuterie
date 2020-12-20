9130a306dadc5a983d09dcc1442190244e0dee76
LaNice Powell <lanicemp@aol.com>
2020-12-20 09:56:37 -0500

    noted out unused functions

--- rare_charcuterie-app/src/actions/index.js
+++ rare_charcuterie-app/src/actions/index.js
@@ -1,28 +1,28 @@
-export const addAuthor = author => {
-    return {
-      type: 'ADD_AUTHOR',
-      author
-    };
-  };
+// export const addAuthor = author => {
+//     return {
+//       type: 'ADD_AUTHOR',
+//       author
+//     };
+//   };
   
-  export const removeAuthor = id => {
-    return {
-      type: 'REMOVE_AUTHOR',
-      id
-    };
-  };
+//   export const removeAuthor = id => {
+//     return {
+//       type: 'REMOVE_AUTHOR',
+//       id
+//     };
+//   };
   
-  export const addBook = book => {
+export const addItem = item => {
     return {
-      type: 'ADD_BOOK',
-      book
+      type: 'ADD_ITEM',
+      item
     };
-  };
+  };  // 
   
-  export const removeBook = id => {
-    return {
-      type: 'REMOVE_BOOK',
-      id
-    };
-  };
+  // export const removeBook = id => {
+  //   return {
+  //     type: 'REMOVE_BOOK',
+  //     id
+  //   };
+  // };
   
