594df13bae96399472639cb95b0607256ca8de6b
LaNice Powell <lanicemp@aol.com>
2020-12-28 18:20:46 -0500

    Inital commit created Card

---
+++ rare_charcuterie-app/src/components/Rating/RatingCard.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Card, Button, Modal, Container, Col, Row } from "react-bootstrap";
+
+class RatingCard extends Component {
+  constructor(props) {
+    super(props);
+    // this.returnViewRating = this.returnViewRating.bind(this);
+  }
+  render() {
+    let { rating } = this.props;
+    console.log(rating)
+    return (
+      <div className="reviews">
+          <Card>
+        <h3 Reviews />
+
+        <div key={rating.id}>
+          <h2>{rating.comment}</h2>
+        </div>
+        </Card>
+      </div>
+    );
+  }
+}
+export default RatingCard;
