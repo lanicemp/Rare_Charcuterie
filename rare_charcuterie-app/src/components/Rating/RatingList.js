4c228c7c1316eb7908ab8b44897c3a726d17595c
LaNice Powell <lanicemp@aol.com>
2020-12-28 18:20:13 -0500

    Cleaned up Rating list to only Render

--- rare_charcuterie-app/src/components/Rating/RatingList.js
+++ rare_charcuterie-app/src/components/Rating/RatingList.js
@@ -1,29 +1,43 @@
 import React, { Component } from "react";
 import { Card, Button, Modal, Carousel } from "react-bootstrap";
+import RatingCard from './RatingCard'
 
-class RatingList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  renderRatings = () => {
-    return this.props.ratings.map((rating) => {
-      console.log("line 14 RatingList", rating);
-      return (
-        <div className="reviews">
-          <h3 Reviews />
-          
-            <div key={rating.id}>
-              <h2>{rating.comment}</h2>
-            </div>
-          
-        </div>
-      );
-    });
-  };
-  render() {
-    return <div>{this.renderRatings()}</div>;
-  }
+const RatingList = ({ratings})=>{
+  console.log(ratings)
+  return ratings.map((rating) => (rating? <RatingCard rating = {rating}/>: 'Loading...')
+  )
 }
+
+
+
+
+
+
+
+
+// class RatingList extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   renderRatings = () => {
+//     return this.props.ratings.map((rating) => {
+//       console.log("line 14 RatingList", rating);
+//       return (
+//         <div className="reviews">
+//           <h3 Reviews />
+          
+//             <div key={rating.id}>
+//               <h2>{rating.comment}</h2>
+//             </div>
+          
+//         </div>
+//       );
+//     });
+//   };
+//   render() {
+//     return <div>{this.renderRatings()}</div>;
+//   }
+// }
 
 export default RatingList;
