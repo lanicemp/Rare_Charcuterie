155eb72d45f4d4ace2c4679826808f340892936b
LaNice Powell <lanicemp@aol.com>
2020-12-31 12:31:15 -0500

    created toItemModal function

--- rare_charcuterie-app/src/components/ItemList/ItemCard.js
+++ rare_charcuterie-app/src/components/ItemList/ItemCard.js
@@ -1,11 +1,13 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import { Card, Button, Modal, Container, Col, Row } from "react-bootstrap";
-import ItemModalCard from './ItemModalCard'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { withRouter, Redirect } from "react-router-dom";
+import ItemModalCard from "./ItemModalCard";
 
 class ItemCard extends Component {
   constructor(props) {
     super(props);
-    // this.returnViewItem = this.returnViewItem.bind(this);
+    this.toItemModal = this.toItemModal.bind(this);
     // this.handleClose = this.handleClose.bind(this);
     // this.renderItem = this.renderItem.bind(this);
 
@@ -13,12 +15,13 @@ class ItemCard extends Component {
       showModal: false,
       item_id: null,
       selectedItem: null,
+      redirect: null
     };
   }
 
   render() {
-      let {item} = this.props
-      console.log("in ItemCard")
+    let { item } = this.props;
+    console.log("in ItemCard");
     return (
       <div className="itemList" id={item.id} data-id={item.id}>
         <Card key={item.id} style={{ width: "25rem" }}>
@@ -30,7 +33,7 @@ class ItemCard extends Component {
               data-id={item.id}
               id={item.id}
               value={item.id}
-              onClick={<ItemModalCard item = {item}/> }
+              onClick={this.toItemModal}
             >
               {" "}
               View Item{" "}
@@ -41,10 +44,28 @@ class ItemCard extends Component {
     );
   }
 
+   toItemModal = (item) => {
+    console.log("in to item modal");
+    console.log(this.props.item.id);
+    console.log(this.props.item)
+    console.log(parseInt(item.target.id))
+  console.log(this.state.showModal)
+  
+    
+    return (
+    
+      <div className="itemModal" key={item.id}>
+          {this.state.showModal == true ? this.showModal() : null}
+        <ItemModalCard  item={this.props.item}/>
+      </div>
+    );
+    
+  }
+
   // returnViewItem(e) {
   //   const id = parseInt(e.target.id);
   //   const foundItem = this.props.items.find((item) => item.id === id);
- 
+
   //   this.myRef = React.createRef.bind(id);
   //   console.log(this.props.items);
   //   this.setState({ showModal: true, item_id: id, selectedItem: foundItem });
@@ -74,57 +95,57 @@ class ItemCard extends Component {
   //   this.setState({ showModal: false });
   // }
 
-  // showModal = () => {
-  //   return (
-  //     <Modal
-  //       //  style={{width: "50rem"} }
-  //       size="lg"
-  //       show={this.state.showModal}
-  //       onHide={() => this.handleClose()}
-  //       //the other syntax of onHide ={this.handleClose()} created an error of cannot exceed mximum calls to state
-  //       // changing the call to an arrow function allowed me to get pass this because the functions is
-  //       //being called in the rended instead of being apart of the render.
-  //       backdrop="static"
-  //       keyboard={false}
-  //     >
-  //       <Modal.Header closeButton>
-  //         <h2>{this.state.selectedItem.name} </h2>
-  //         {console.log(this.state)}
-  //       </Modal.Header>
-  //       <Modal.Body>{this.renderItem(this.props.item.id)} </Modal.Body>
-  //       <Modal.Footer>
-  //         <Button variant="secondary" onClick={() => this.handleClose()}>
-  //           Close
-  //         </Button>
-  //       </Modal.Footer>{" "}
-  //     </Modal>
-  //   );
-  // };
- 
+  showModal = () => {
+    debugger
+    return (
+      <Modal
+        //  style={{width: "50rem"} }
+        size="lg"
+        show={this.state.showModal}
+        onHide={() => this.handleClose()}
+        //the other syntax of onHide ={this.handleClose()} created an error of cannot exceed mximum calls to state
+        // changing the call to an arrow function allowed me to get pass this because the functions is
+        //being called in the rended instead of being apart of the render.
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <h2>{this.state.selectedItem.name} </h2>
+          {console.log(this.state)}
+        </Modal.Header>
+        <Modal.Body>{this.renderItem(this.props.item.id)} </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => this.handleClose()}>
+            Close
+          </Button>
+        </Modal.Footer>{" "}
+      </Modal>
+    );
+  };
+
   // renderItem = (props,item_id) => {
   //   let item = this.props.item
   //   console.log(item)
-   
+
   //   return (
   //     <div className="viewItem" id={item_id} data-id={item_id}>
   //     <Container>
   //     <Row>
   //     <Col xs={9}>
   //         <img key="img" variant="top" src={item.img_url} />
-  //         </Col> 
-  //         <Col> 
+  //         </Col>
+  //         <Col>
   //         <h2> $ {item.price}</h2>
   //         <h6> Ingredients:  {item.ingredient}</h6>
   //         <h4> Serving Size: {item.size}</h4>
   //         </Col>
   //           {/* <h2>{item.name}</h2>
   //           <h3>$ {item.price}</h3> */}
-  //           </Row> 
+  //           </Row>
   //         </Container>
   //     </div>
   //   );
   // };
-
 }
 
 export default ItemCard;
